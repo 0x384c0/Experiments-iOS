2e3e0588a35f7057456564e8f84862c0bc5fff84
0x384c0 <0x384c0@gmail.com>
2021-04-23 15:43:59 +0600

    Update get_version_from_tag.sh

--- scripts/others/get_version_from_tag.sh
+++ scripts/others/get_version_from_tag.sh
@@ -1,4 +1,26 @@
-VERSION_STR=$(git describe --abbrev=0)
-if  [[ $VERSION_STR = *[!\ ]* ]]; then
-	echo $VERSION_STR > VERSION
+#!/bin/sh
+function banner {
+    if [ -z "$TERM" ]; then >&2 echo "######## $1 #######";
+    else >&2 echo "$(tput setaf 5; tput bold;)######## $1 #######$(tput sgr0)"; fi
+}
+function info {
+    if [ -z "$TERM" ];then >&2 echo "INFO: $1";
+    else >&2 echo "$(tput setaf 2; tput bold;)INFO: $1$(tput sgr0)"; fi
+}
+function warn {
+    if [ -z "$TERM" ];then >&2 echo "WARNING: $1";
+    else >&2 echo "$(tput setaf 3; tput bold;)WARNING: $1$(tput sgr0)"; fi
+}
+function error {
+    if [ -z "$TERM" ];then >&2 echo "ERROR: $1";
+    else >&2 echo "$(tput setaf 1; tput bold;)ERROR: $1$(tput sgr0)"; fi
+}
+
+VERSION_STR=$(git tag --sort=-creatordate | head -1)
+
+if [[ "$VERSION_STR" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo $VERSION_STR
+else
+    error "Illegal version: $VERSION_STR. Should match ^[0-9]+\.[0-9]+\.[0-9]+\$"
+    exit 1
 fi
