020b9d1ae70ec6a6288bd3ceefb9e92f4e03a611
0x384c0 <0x384c0@gmail.com>
2021-04-23 16:50:37 +0600

    Update set_version.sh

--- scripts/others/set_version.sh
+++ scripts/others/set_version.sh
@@ -1,40 +1,32 @@
+#!/bin/sh
 function banner {
-    if [ -z "$TERM" ]; then echo "######## $1 #######";
-    else echo "$(tput setaf 5; tput bold;)######## $1 #######$(tput sgr0)"; fi
+    if [ -z "$TERM" ]; then >&2 echo "######## $1 #######";
+    else >&2 echo "$(tput setaf 5; tput bold;)######## $1 #######$(tput sgr0)"; fi
 }
 function info {
-    if [ -z "$TERM" ];then echo "INFO: $1";
-    else echo "$(tput setaf 2; tput bold;)INFO: $1$(tput sgr0)"; fi
+    if [ -z "$TERM" ];then >&2 echo "INFO: $1";
+    else >&2 echo "$(tput setaf 2; tput bold;)INFO: $1$(tput sgr0)"; fi
+}
+function warn {
+    if [ -z "$TERM" ];then >&2 echo "WARNING: $1";
+    else >&2 echo "$(tput setaf 3; tput bold;)WARNING: $1$(tput sgr0)"; fi
+}
+function error {
+    if [ -z "$TERM" ];then >&2 echo "ERROR: $1";
+    else >&2 echo "$(tput setaf 1; tput bold;)ERROR: $1$(tput sgr0)"; fi
 }
 
-set -e
-VERSION_STR=$(cat VERSION)
-PROJECT=IOSExperiments
+VERSION_STR=$1
 
-semver=( ${VERSION_STR//./ } )
-major="${semver[0]}"
-minor="${semver[1]}"
-patch="${semver[2]}"
-
-BUNDLE_VERSION=$patch
-SHORT_BUNDLE_VERSION="${major}.${minor}"
-banner "Will set version for $PROJECT"
-info "major:    $major"
-info "minor:    $minor"
-info "patch:    $patch"
-info "BUNDLE_VERSION:        $BUNDLE_VERSION"
-info "SHORT_BUNDLE_VERSION:  $SHORT_BUNDLE_VERSION"
-
-if [[ $BUNDLE_VERSION = *[!\ ]* ]]; then
-    plutil -replace CFBundleVersion -string $BUNDLE_VERSION $PROJECT/Info.plist
-    plutil -replace CFBundleVersion -string $BUNDLE_VERSION $PROJECT/Dev-Info.plist
-
-    plutil -replace CFBundleShortVersionString -string $SHORT_BUNDLE_VERSION $PROJECT/Info.plist
-    plutil -replace CFBundleShortVersionString -string $SHORT_BUNDLE_VERSION $PROJECT/Dev-Info.plist
-
+if [[ "$VERSION_STR" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    DISPLAY_VERSION=$(echo $VERSION_STR | grep -o -E ^[0-9]+\.[0-9]+) &&\
+    BUILD_NUMBER=$(echo $VERSION_STR | grep -o -E [0-9]+$) &&\
+    fastlane run increment_version_number version_number:$DISPLAY_VERSION  &&\
+    fastlane run increment_build_number build_number:$BUILD_NUMBER
+    
     #   Output version & build numbers into a label on LaunchScreen.storyboard
-	# sed -i bak -e "/userLabel=\"APP_VERSION\"/s/text=\"[^\"]*\"/text=\"$VERSION_STR\"/" $PROJECT/Base.lproj/LaunchScreen.storyboard
-
+    # sed -i bak -e "/userLabel=\"APP_VERSION\"/s/text=\"[^\"]*\"/text=\"$VERSION_STR\"/" $PROJECT/Base.lproj/LaunchScreen.storyboard
+else
+    error "Illegal version: $VERSION_STR. Should match ^[0-9]+\.[0-9]+\.[0-9]+\$"
+    exit 1
 fi
-
-banner DONE
