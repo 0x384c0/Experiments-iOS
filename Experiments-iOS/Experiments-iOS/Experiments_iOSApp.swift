88bc9f34ce54c73d8dda542939de72a5667d0db0
0x384c0 <0x384c0@gmail.com>
2023-03-27 11:24:17 +0600

    added ios project

---
+++ Experiments-iOS/Experiments-iOS/Experiments_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Experiments_iOSApp.swift
+//  Experiments-iOS
+//
+//  Created by 0x384c0 on 27/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Experiments_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
